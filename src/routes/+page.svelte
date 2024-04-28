dc331331a285f44ecdf5c3851c03c94dbb5977c1
Hrelotem <94904267+Hrelotem@users.noreply.github.com>
2024-04-28 11:58:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  function personale() {
+    window.location.href = "/loginpersonale";
+  }
+
+  function patient() {
+    window.location.href = "/loginpatient";
+  }
+</script>
+
+<h1>Velkommen</h1>
+<p>Er du personale eller borger?</p>
+
+<button on:click={patient}>Patient</button>
+<button on:click={personale}>Personale</button>
+
